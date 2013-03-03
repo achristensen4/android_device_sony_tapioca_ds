9682dc5df2bacff85179694c215b778fcd6bc4bc
Alin Jerpelea <jerpelea@gmail.com>
2013-03-03 11:26:03 +0200

    tapioca_ds: typo fix
    Change-Id: Ib6011f937570e1a08c9809a6098bfeeb5aae81ca

--- cm.mk
+++ cm.mk
@@ -6,8 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST21ii_1257-4009 BUILD_FINGERPRINT=SEMC/ST21ii_1257-4009/ST21ii:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST21ii
--user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ST21ii_1257-4009 BUILD_FINGERPRINT=SEMC/ST21ii_1257-4009/ST21ii:4.0.4/6.1.A.0.452/O_5_zw:user/release-keys PRIVATE_BUILD_DESC="ST21ii-user 4.0.4 6.1.A.0.452 O_5_zw test-keys"
 
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
